8504a140a2be181b7441faf27df5a9b3306e7719
kingsman8888 <youchihun@gmail.com>
2021-10-23 18:39:39 +0900

    update java88

--- src/ch14/_01_ThreadMain.java
+++ src/ch14/_01_ThreadMain.java
@@ -7,9 +7,11 @@ public class _01_ThreadMain {
 		/*
 		 * [면접] Thread 문법 1) Thread 클래스를상속하기
 		 * 
-		 * 1. Thread 클래스를 상속 받아서 클래스를 작성한다. 2. run() 메서드를 재정의(Override) 한다. 3. main()
-		 * 메서드에서 Thread 객체를 생성한다. 4. start() 메서드를 호출해서 스레드를 실행한다. ( 호출은 start)로한다. 5.
-		 * run() 메서드가 실행된다.
+		 * 1. Thread 클래스를 상속 받아서 클래스를 작성한다. 
+		 * 2. run() 메서드를 재정의(Override) 한다. 
+		 * 3. main()  메서드에서 Thread 객체를 생성한다. 
+		 * 4. start() 메서드를 호출해서 스레드를 실행한다. ( 호출은 start)로한다. 
+		 * 5. run() 메서드가 실행된다.
 		 * 
 		 */
 
